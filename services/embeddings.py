4b0c84e4fff3d99262295b4ed7b01cdf8eb4c374
Anukul-Chandra <anukulchandra5585@gmail.com>
2026-07-02 02:27:37 +0600

    embedding service added

---
+++ services/embeddings.py
@@ -0,0 +1,15 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+
+
+_model = None
+
+
+def get_embedding_model() -> HuggingFaceEmbeddings:
+    global _model
+
+    if _model is None:
+        _model = HuggingFaceEmbeddings(
+            model_name="sentence-transformers/all-MiniLM-L6-v2"
+        )
+
+    return _model
